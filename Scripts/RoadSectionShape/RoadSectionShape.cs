1493fbc3f5ba29c2b431d2eef70535f84f881b30
Johnny Howe <jonathon.o.howe@gmail.com>
2026-05-13 21:14:14 +1200

    Update RoadSectionShape.cs

--- Scripts/RoadSectionShape/RoadSectionShape.cs
+++ Scripts/RoadSectionShape/RoadSectionShape.cs
@@ -1,8 +1,5 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using JonathonOH.RoadGeneration.ConvexShape2D;
-using Other;
 using UnityEngine;
 
 namespace JonathonOH.RoadGeneration
