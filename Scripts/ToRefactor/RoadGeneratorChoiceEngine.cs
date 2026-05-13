7ae91122a0267831165362e4779a08ecf38e943e
Johnny Howe <jonathon.o.howe@gmail.com>
2026-05-13 15:46:48 +1200

    Better overlap logging

--- Scripts/ToRefactor/RoadGeneratorChoiceEngine.cs
+++ Scripts/ToRefactor/RoadGeneratorChoiceEngine.cs
@@ -87,8 +87,8 @@ namespace JonathonOH.RoadGeneration
 			if (collisionCheckResult.HasCollision)
 			{
 				Debug.Log(
-					$"Invalid chain found. Candidate {collisionCheckResult.Request.Subject} overlaps with {collisionCheckResult.CollidesWith}\n" + 
-					$"Full chain: {collisionCheckResult.Request.GetFullChain()}"
+					$"Invalid chain found. Candidate {collisionCheckResult.Request.Subject.gameObject.name} overlaps with {collisionCheckResult.CollidesWith.gameObject.name}\n" + 
+					$"Full chain: {string.Join(", ", collisionCheckResult.Request.GetFullChain().Select(section => section.gameObject.name))}"
 				);
 				combinationGenerator.StepInvalid();
 			}
