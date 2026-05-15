4517b46f09a944f7881f23039e17ba6d097b25cb
Johnny Howe <jonathon.o.howe@gmail.com>
2026-05-15 19:19:20 +1200

    Created failing tangent tests for very small differences in floats

--- Scripts/ConvexShape2D/Tests/ConvexHullUtility/GetTangentsFromHull/GetTangentsFromHullDegenerateCaseTests.cs
+++ Scripts/ConvexShape2D/Tests/ConvexHullUtility/GetTangentsFromHull/GetTangentsFromHullDegenerateCaseTests.cs
@@ -20,13 +20,58 @@ namespace JonathonOH.RoadGeneration.ConvexShape2D.Tests.ConvexHullUtilityTests
 				new Vector2(0f, 0f)
 			};
 
-			Vector2[] result = ConvexHullUtility.GetTangentsFromHull(hullClockwise).ToArray();
-
-			Assert.That(result, Is.EqualTo(new[]
+			Vector2[] expected = new[]
 			{
 				Vector2.right,
-				Vector2.down
-			}).Using(Vector2EqualityComparer.Instance));
+				Vector2.up
+			};
+
+			Vector2[] result = ConvexHullUtility.GetTangentsFromHull(hullClockwise).ToArray();
+			Assert.That(result, Is.EqualTo(expected).Using(Vector2EqualityComparer.Instance));
+		}
+
+		[Test]
+		public void AdjacentVerticesThatNormalizeToZero_SkipsEdge()
+		{
+			Vector2[] hullClockwise =
+			{
+				new Vector2(0f, 6.520003f),
+				new Vector2(1f, 6.520003f),
+				new Vector2(1f, 6.520002f),
+				new Vector2(1f, 0f),
+				new Vector2(0f, 0f),
+			};
+
+			Vector2[] expected = new[]
+			{
+				Vector2.right,
+				Vector2.up
+			};
+
+			Vector2[] result = ConvexHullUtility.GetTangentsFromHull(hullClockwise).ToArray();
+			Assert.That(result, Is.EqualTo(expected).Using(Vector2EqualityComparer.Instance));
+		}
+
+
+		[Test]
+		public void ClosingVerticesThatNormalizeToZero_SkipsEdge()
+		{
+			Vector2[] hullClockwise =
+			{
+				new Vector2(3.48f, 3.480001f),
+				new Vector2(3.480002f, -3.479997f),
+				new Vector2(-3.479998f, -3.479998f),
+				new Vector2(-3.48f, 3.48f)
+			};
+
+			Vector2[] expected = new[]
+			{
+				Vector2.right,
+				Vector2.up
+			};
+
+			Vector2[] result = ConvexHullUtility.GetTangentsFromHull(hullClockwise).ToArray();
+			Assert.That(result, Is.EqualTo(expected).Using(Vector2EqualityComparer.Instance));
 		}
 	}
 }
