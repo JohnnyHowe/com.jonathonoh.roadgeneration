50564c5e1edfb9c339cf448d252b97b6eed16a0f
Johnny Howe <jonathon.o.howe@gmail.com>
2026-05-18 10:26:02 +1200

    Created PoseSpaceTransformationExtensionsTests skeleton

---
+++ Scripts/JonathonOH.Spatial/Tests/PoseSpaceTransformationExtensionsTests/TransformPoseTests.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+
+namespace JonathonOH.Spatial.Tests.TransformPoseTests
+{
+	public static class TransformPoseTests
+	{
+		// Test cases to create for Pose.TransformPose
+		// TODO
+
+		[Test]
+		public static void Test()
+		{
+
+		}
+	}
+}
