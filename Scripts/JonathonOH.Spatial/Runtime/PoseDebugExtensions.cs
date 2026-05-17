343037c5fbe88c14b6ff15ce15d3973b45b629a1
Johnny Howe <jonathon.o.howe@gmail.com>
2026-05-18 10:46:56 +1200

    Implemented PoseDebugExtensions

--- Scripts/JonathonOH.Spatial/Runtime/PoseDebugExtensions.cs
+++ Scripts/JonathonOH.Spatial/Runtime/PoseDebugExtensions.cs
@@ -1,18 +1,31 @@
-using System;
 using UnityEngine;
 
 namespace JonathonOH.Spatial
 {
 	public static class PoseDebugExtensions
 	{
+		private const float arrowHeadLength = 0.1f;
+		private const float arrowLength = 0.4f;
+		private const float arrowHeadAngle = 20;
+
 		public static void DebugDraw(this Pose pose)
 		{
-			throw new NotImplementedException();
+			DrawArrow(pose.position, pose.forward, Color.blue);
+			DrawArrow(pose.position, pose.right, Color.red);
+			DrawArrow(pose.position, pose.up, Color.green);
 		}
 
-		public static void DebugDraw(this Pose pose, Color color)
+		private static void DrawArrow(Vector3 start, Vector3 direction, Color color)
 		{
-			throw new NotImplementedException();
+			Vector3 end = start + direction;
+			direction *= arrowLength;
+
+			Debug.DrawRay(start, direction, color);
+
+			Vector3 rightArrowHeadLine = Quaternion.LookRotation(direction) * Quaternion.Euler(0, 180 + arrowHeadAngle, 0) * new Vector3(0, 0, 1);
+			Vector3 leftArrowHeadLine = Quaternion.LookRotation(direction) * Quaternion.Euler(0, 180 - arrowHeadAngle, 0) * new Vector3(0, 0, 1);
+			Debug.DrawRay(end, rightArrowHeadLine * arrowHeadLength, color);
+			Debug.DrawRay(end, leftArrowHeadLine * arrowHeadLength, color);
 		}
 	}
 }
