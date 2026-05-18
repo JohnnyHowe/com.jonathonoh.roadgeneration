c88e94b33574a2c6edd03ebd111f6d3fc7d144c1
Johnny Howe <jonathon.o.howe@gmail.com>
2026-05-18 22:47:50 +1200

    Created skeleton NoPartition for initial implementation

---
+++ Scripts/Spatial/StaticPartitioning2D/NoPartition/Runtime/NoPartition.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+namespace JonathonOH.Spatial.StaticPartitioning2D
+{
+	public class NoPartition: IPartition
+	{
+		public void SetAllObjects(IEnumerable<IPartitionable> objects)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Add(IPartitionable partitionable)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Remove(IPartitionable partitionable)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IPartitionable GetFirstObjectWithOverlap(IPartitionable other)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
