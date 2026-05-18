131d85052737f6b22ebcb35a310b014c7798e59b
Johnny Howe <jonathon.o.howe@gmail.com>
2026-05-18 22:06:54 +1200

    Removed redundant ConvexPolygon.GetBoundingRect

--- Scripts/Geometry/ConvexPolygon/Runtime/ConvexPolygon.cs
+++ Scripts/Geometry/ConvexPolygon/Runtime/ConvexPolygon.cs
@@ -60,25 +60,7 @@ namespace JonathonOH.Geometry
 
 		public override string ToString()
 		{
-			Rect bounds = GetBoundingRect();
-			return $"ConvexHull<Center={bounds.center}, size={bounds.size}>";
-		}
-
-		public Rect GetBoundingRect()
-		{
-			if (Vertices.Count == 0)
-				return new Rect();
-
-			Vector2 min = Vertices[0];
-			Vector2 max = Vertices[0];
-
-			for (int i = 1; i < Vertices.Count; i++)
-			{
-				min = Vector2.Min(min, Vertices[i]);
-				max = Vector2.Max(max, Vertices[i]);
-			}
-
-			return Rect.MinMaxRect(min.x, min.y, max.x, max.y);
+			return $"ConvexHull<Center={BoundingRect.center}, size={BoundingRect.size}>";
 		}
 	}
 }
